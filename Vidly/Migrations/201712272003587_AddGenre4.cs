d68e0355c193709dbe153871f54ad272ee2c61d4
johnnycnm <johnnya@completenetwork.com>
2018-01-16 10:59:43 -0600

    Fisrt Push

---
+++ Vidly/Migrations/201712272003587_AddGenre4.cs
@@ -0,0 +1,40 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddGenre4 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Movies", "NumberInStock", c => c.Byte(nullable: false));
+            AddColumn("dbo.Movies", "Genre_Id", c => c.Byte(nullable: false));
+            AlterColumn("dbo.Movies", "Name", c => c.String(nullable: false, maxLength: 255));
+            AlterColumn("dbo.Movies", "GenreId", c => c.String());
+            CreateIndex("dbo.Movies", "Genre_Id");
+            AddForeignKey("dbo.Movies", "Genre_Id", "dbo.Genres", "Id", cascadeDelete: true);
+            DropColumn("dbo.Movies", "Stock");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Movies", "Stock", c => c.Byte(nullable: false));
+            DropForeignKey("dbo.Movies", "Genre_Id", "dbo.Genres");
+            DropIndex("dbo.Movies", new[] { "Genre_Id" });
+            AlterColumn("dbo.Movies", "GenreId", c => c.String(nullable: false));
+            AlterColumn("dbo.Movies", "Name", c => c.String());
+            DropColumn("dbo.Movies", "Genre_Id");
+            DropColumn("dbo.Movies", "NumberInStock");
+            DropTable("dbo.Genres");
+        }
+    }
+}
